cb4cd4ddeb83ae30d1b18d1288a79b4eb85f6d5d
joeldevel <christianjoel.developer@gmail.com>
2021-09-15 18:00:19 -0300

    add variables example

---
+++ variables.js
@@ -0,0 +1,11 @@
+// There are three ways of declaring a variable
+
+// var, let and const (which is not variable after all)
+
+var myTelephoneNumber = 123456;
+
+let myAddress = "St. Martin 112";
+
+const myId = 25696888;
+
+console.log(myTelephoneNumber, myAddress, myId);
