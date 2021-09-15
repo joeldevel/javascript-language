4d3a96c237f900aa60f711ebab6ea3d0d8ad8c75
joeldevel <christianjoel.developer@gmail.com>
2021-09-15 18:40:52 -0300

    add double vs triple equals example

---
+++ check_equality.js
@@ -0,0 +1,11 @@
+// '===' operator checks both type and value
+// '==' operator checks only value
+
+let aNumber = 300;
+let aStringNumber = '300';
+
+console.log("aNumber: ", aNumber);
+console.log("aStringNumber: ", aStringNumber);
+
+console.log("aNumber == aStringNumber?",aNumber == aStringNumber);
+console.log("aNumber === aStringNumber?",aNumber === aStringNumber);
